ccfcbeebef0db1efe608e0617e15e2f91d4b1fb1
chamo <137000540+chamo2002@users.noreply.github.com>
2024-04-10 11:31:26 +0530

    Add files via upload

---
+++ Lab3 Question2/Lab3 Question2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Question2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(101, "John Doe", 50000.00);
+
+            Console.WriteLine("Employee Information ");
+            employee.DisplayEmployeeInfo();
+
+            Console.WriteLine($"Employee ID {employee.EmployeedID}");
+
+            employee.FullName = "Jane Smith";
+            Console.WriteLine($"Updated Full Name {employee.FullName}");
+
+            employee.Salary = 55000;
+            Console.WriteLine($"Updated salary  {employee.salary}");
+
+            Console.ReadLine();
+        }
+    }
+}
+
+internal class Employee
+{
+    public int employeeID;
+    public string fullName;
+    public double salary;
+
+    public Employee(int employeeID, string FullName, double salary)
+    {
+        this.employeeID = employeeID;
+        this.fullName = FullName;
+        this.salary = salary;
+    }
+    public int EmployeedID => employeeID;
+
+    public string FullName
+    {
+        get => FullName;
+        set => FullName = value;
+
+    }
+    public double Salary
+    {
+        get => salary;
+        set => salary = value;
+    }
+
+    public void DisplayEmployeeInfo()
+    {
+        Console.WriteLine($"Employee ID {employeeID}");
+        Console.WriteLine($"Full Name {employeeID}");
+        Console.WriteLine($"Salary {employeeID}");
+    }
+}
